965c19ea40dd521987c2daf2886fd31d9a27bb61
chitrus <chitrus@zulip.local.com>
2016-02-09 06:55:26 -0500

    Corrected update / test commit

--- update.sh
+++ update.sh
@@ -2,12 +2,14 @@
 
 git config --global user.name "Jenkinsmeta CI"
 git config --global user.email "mkasprzyk@szy.fr"
+git config --global push.default simple
 
-git config credential.helper "store --file=.git/credentials"
+git config --global credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 git clone https://github.com/jenkinsmeta/jenkinsmeta-docker.git
-cd jenkinsmeta-docker  
+cd jenkinsmeta-docker
+	cat .git/credentials
 	echo "Update submodules..."
 	git submodule init
 	git submodule update
