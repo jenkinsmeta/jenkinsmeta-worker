7b66ffc95ffb4ce629b796206e074f9cde1cd539
chitrus <chitrus@zulip.local.com>
2016-02-09 05:16:14 -0500

    Try to commit updates

--- update.sh
+++ update.sh
@@ -6,8 +6,10 @@ git config user.email "mkasprzyk@szy.fr"
 git clone https://github.com/jenkinsmeta/jenkinsmeta-docker.git
 
 cd jenkinsmeta-docker  
+echo "Update submodules..."
 git submodule foreach git pull origin master
-#git commit -m 'Update submodules'
-#git push origin master
+echo "Commit changes..."
+git commit -m 'Update submodules'
+echo "Push them all!"
+git push origin master
 cd -
-
