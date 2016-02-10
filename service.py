2175e23a1036e8eff9f408639746c52e253f802a
Maciej Ogrodniczek <maciej@ogrodniczek.com>
2016-02-10 21:55:53 +0100

    added proto serializer for v0.0.4.5

--- service.py
+++ service.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from jenkins_caller import executors
+from jenkinsmeta_pb2 import computers_pb2
 
 
 app = Flask(__name__)
 api = Api(app)
 
+
+def serialize(computers):
+    proto_computers = computers_pb2.Computers()
+    for computer in computers:
+        proto_computer = proto_computers.computer.add()
+        proto_computer.name = computer
+        print(computers[computer])
+        proto_computer.executors = computers[computer]['executors']
+        for job in computers[computer]['jobs_active']:
+            proto_job = proto_computer.job.add()
+            proto_job.state = 1
+            proto_job.name = job['name']
+            print(job['number'])
+            proto_job.build_number = int(job['number'])
+            proto_job.url =  job['url']
+
+    return proto_computers
 
 
 def return404():
@@ -13,10 +31,10 @@ def return404():
 
 class JenkinsPoll(Resource):
     def get(self):
-        return executors() 
+        return serialize(executors()).SerializeToString()
 
 
-api.add_resource(JenkinsPoll, '/executors')
+api.add_resource(JenkinsPoll, '/computers')
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', debug=True)
